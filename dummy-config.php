13676a1fd401133993bf048a4836918ef0d37665
Matheus Mathias <mathias.matheus@ymail.com>
2026-07-28 22:17:15 -0300

    feat: update volume mapping in local.yml

--- dummy-config.php
+++ dummy-config.php
@@ -1,6 +1,7 @@
 <?php
 // Arquivo de redirecionamento para o ambiente Docker de desenvolvimento.
 // Como os plugins são symlinks a partir de /home/matheus/moodle-dev/meus-plugins,
-// os caminhos relativos como ../../config.php resolvem para /home/matheus/config.php.
+// os caminhos relativos como ../../config.php resolvem para /home/matheus/moodle-dev/config.php
+// e ../../../config.php resolvem para /home/matheus/config.php.
 // Este arquivo direciona para o config real.
 require_once('/var/www/html/config.php');
